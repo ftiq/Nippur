6e676c499fb6dc387bb4a515a68c66ac4ef326d3
ftiq <manar.z@ft.iq>
2025-01-19 13:00:22 +0300

    Add files via upload

---
+++ sales_costemer/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Sales Order Customization",
+    "version": "1.0",
+    "author": "Your Name",
+    "website": "https://yourwebsite.com",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
