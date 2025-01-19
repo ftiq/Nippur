14cf75978d9b7a4f7b4c91cd247f525ebd165e95
ftiq <manar.z@ft.iq>
2025-01-19 13:06:43 +0300

    Delete sales_costemer directory

--- sales_costemer/models/sale_order_line.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields # type: ignore
-
-class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
-
-    gift_quantity = fields.Float(string="Gift Quantity", default=0.0)
