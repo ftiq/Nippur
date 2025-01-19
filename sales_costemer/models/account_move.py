51557c47bb8bd7ee827bfab66fcf600058841b9b
ftiq <manar.z@ft.iq>
2025-01-19 12:54:39 +0300

    Delete sales_costemer directory

--- sales_costemer/models/account_move.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
-
-    gift_quantity = fields.Float(string="Gift Quantity", default=0.0)
--- sales_costemer/models/sale_order.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
-
-    representative_id = fields.Many2one('res.users', string="Sales Representative")
