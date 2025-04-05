a5dfadb446d21619fc86e67758fc08ec9fa5be20
MHD Manar Zamrik <manar.z@ft.iq>
2025-04-05 21:48:42 +0300

    Update account_move_line.py

--- account_line_remaining_due/models/account_move_line.py
+++ account_line_remaining_due/models/account_move_line.py
@@ -1,17 +1,36 @@
-@api.depends('move_id.line_ids.amount_residual')
-def _compute_remaining_due(self):
-    for line in self:
-        if line.account_id.account_type == 'income' and line.move_id:
-            move = line.move_id
-            customer_line = move.line_ids.filtered(lambda l: l.account_id.account_type == 'asset_receivable')
-            sales_lines = move.line_ids.filtered(lambda l: l.account_id.account_type == 'income')
+from odoo import api, fields, models
 
-            if customer_line and sales_lines:
-                total_credit = sum(l.credit for l in sales_lines)
-                residual = customer_line[0].amount_residual
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
 
-                line.remaining_due = (line.credit / total_credit) * residual if total_credit else 0.0
+    remaining_due = fields.Monetary(
+        string='Remaining Due',
+        currency_field='currency_id',
+        compute='_compute_remaining_due',
+        store=True
+    )
+
+    @api.depends('move_id.line_ids.amount_residual')
+    def _compute_remaining_due(self):
+        for line in self:
+            # فقط على خطوط المبيعات
+            if line.account_id.account_type == 'income' and line.move_id:
+                move = line.move_id
+
+                # سطر الزبون
+                customer_line = move.line_ids.filtered(
+                    lambda l: l.account_id.account_type == 'asset_receivable'
+                )
+                # كل خطوط المبيعات
+                sales_lines = move.line_ids.filtered(
+                    lambda l: l.account_id.account_type == 'income'
+                )
+
+                if customer_line and sales_lines:
+                    total_credit = sum(l.credit for l in sales_lines)
+                    residual = customer_line[0].amount_residual
+                    line.remaining_due = (line.credit / total_credit) * residual if total_credit else 0.0
+                else:
+                    line.remaining_due = 0.0
             else:
                 line.remaining_due = 0.0
-        else:
-            line.remaining_due = 0.0
