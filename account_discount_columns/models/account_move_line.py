ecbfdf8a295300fee5e2e57ace7852927602d0b4
MHD Manar Zamrik <manar.z@ft.iq>
2025-08-20 20:25:31 +0300

    Update account_move_line.py

--- account_discount_columns/models/account_move_line.py
+++ account_discount_columns/models/account_move_line.py
@@ -1,42 +1,22 @@
 from odoo import api, fields, models
 
-class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
+class AccountMove(models.Model):
+    _inherit = 'account.move'
 
-    discount_amount = fields.Monetary(
-        string='Discount Amount',
-        compute='_compute_discount_amounts',
+    total_discount = fields.Monetary(
+        string="إجمالي الخصم",
+        compute="_compute_total_discount",
         store=True,
-        currency_field='currency_id',
+        currency_field="currency_id"
     )
 
-    gross_total = fields.Monetary(
-        string='Gross Total',
-        compute='_compute_discount_amounts',
-        store=True,
-        currency_field='currency_id',
-    )
+    @api.depends('invoice_line_ids.product_id', 'invoice_line_ids.price_subtotal')
+    def _compute_total_discount(self):
+        DISCOUNT_PRODUCT_ID = 9   # رقم المنتج الخاص بالخصم
 
-    @api.depends('price_unit', 'quantity', 'discount', 'currency_id')
-    def _compute_discount_amounts(self):
-        for line in self:
-            currency = line.currency_id
-
-            if line.display_type:
-                line.discount_amount = 0.0
-                line.gross_total = 0.0
-                continue
-
-            raw_amount = line.price_unit * line.quantity if line.price_unit and line.quantity else 0.0
-
-            if line.price_unit < 0:  
-                # خصم
-                disc = abs(raw_amount)
-                line.discount_amount = currency.round(disc) if currency else disc
-                line.gross_total = 0.0
-            else:
-                # سطر عادي
-                disc = raw_amount * (line.discount / 100.0) if line.discount else 0.0
-                gross = raw_amount + disc
-                line.discount_amount = currency.round(disc) if currency else disc
-                line.gross_total = currency.round(gross) if currency else gross
+        for move in self:
+            discount_total = 0.0
+            for line in move.invoice_line_ids:
+                if line.product_id and line.product_id.id == DISCOUNT_PRODUCT_ID:
+                    discount_total += abs(line.price_subtotal)
+            move.total_discount = discount_total
